dca269f107092d4ff22184f5672ba2fa1171f6e8
manas gupta <manasgupta0110@gmail.com>
2023-05-31 02:49:16 +0530

    image source changed

--- src/components/body/Carousel.jsx
+++ src/components/body/Carousel.jsx
@@ -4,22 +4,14 @@ function ClientCarousel() {
   return (
     <Carousel>
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://www.pexels.com/photo/black-and-white-photo-of-wild-horses-running-through-valley-14818545/"
-          alt="First slide"
-        />
+        <img className="d-block w-100" src="" alt="First slide" />
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://images.pexels.com/photos/14818545/pexels-photo-14818545.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-          alt="Second slide"
-        />
+        <img className="d-block w-100" src="" alt="Second slide" />
 
         <Carousel.Caption>
           <h3>Second slide label</h3>
@@ -27,11 +19,7 @@ function ClientCarousel() {
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://www.pexels.com/photo/newlywed-couple-holding-hands-16128300/"
-          alt="Third slide"
-        />
+        <img className="d-block w-100" src="" alt="Third slide" />
 
         <Carousel.Caption>
           <h3>Third slide label</h3>
